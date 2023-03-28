170d377f6a9907f37f55bc2d0db93a8a20389049
Mark Christian <mark@serval.sh>
2023-03-28 09:11:41 -0700

    Kaboodle::discover_peers and ::discover_next_peer

--- src/errors.rs
+++ src/errors.rs
@@ -18,4 +18,7 @@ pub enum KaboodleError {
 
     #[error("Unable to stop: {0}")]
     StoppingFailed(String),
+
+    #[error("Functionality unavailable because the mesh is not currently running")]
+    NotRunning,
 }
